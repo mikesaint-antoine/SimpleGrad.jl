96691badf807a51f71487a07ce357c31e6353a05
Mike Saint-Antoine <msa406@nyu.edu>
2024-06-08 13:34:41 -0400

    adding tests

---
+++ test/runtests.jl
@@ -0,0 +1,127 @@
+using SimpleGrad
+using Test
+using Flux
+
+rand_lower = -20
+rand_upper = 20
+
+function random_value(lower, upper)
+    return lower + (upper - lower) * rand()
+end
+
+
+
+
+@testset "Value tests" begin
+
+    # Addition, Value + Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 + num2
+    backward(num3)
+
+    flux_add(a, b) = a + b
+    grad = gradient(flux_add, num1_val, num2_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num2.grad - grad[2]) < 1e-6
+    @test abs(num3.data - (num1_val + num2_val)) < 1e-6
+
+    # Subtraction, Value - Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 - num2
+    backward(num3)
+
+    flux_subtract(a, b) = a - b
+    grad = gradient(flux_subtract, num1_val, num2_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num2.grad - grad[2]) < 1e-6
+    @test abs(num3.data - (num1_val - num2_val)) < 1e-6
+
+    # Multiplication, Value * Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 * num2
+    backward(num3)
+
+    flux_multiply(a, b) = a * b
+    grad = gradient(flux_multiply, num1_val, num2_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num2.grad - grad[2]) < 1e-6
+    @test abs(num3.data - (num1_val * num2_val)) < 1e-6
+
+    # Division, Value / Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 / num2
+    backward(num3)
+
+    flux_divide(a, b) = a / b
+    grad = gradient(flux_divide, num1_val, num2_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num2.grad - grad[2]) < 1e-6
+    @test abs(num3.data - (num1_val / num2_val)) < 1e-6
+
+    # e^x, exp(Value)
+    num1_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = exp(num1)
+    backward(num2)
+
+    flux_exp(a) = exp(a)
+    grad = gradient(flux_exp, num1_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num2.data - exp(num1_val)) < 1e-6
+
+    # natural log, log(Value)
+    num1_val = random_value(0, rand_upper)
+    num1 = Value(num1_val)
+    num2 = log(num1)
+    backward(num2)
+
+    flux_log(a) = log(a)
+    grad = gradient(flux_log, num1_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num2.data - log(num1_val)) < 1e-6
+
+    # x^c, Value^c
+    num1_val = random_value(0, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper) * 0.1
+    num1 = Value(num1_val)
+    num3 = num1 ^ num2_val
+    backward(num3)
+
+    flux_power(a, b) = a ^ b
+    grad = gradient(flux_power, num1_val, num2_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num3.data - (num1_val ^ num2_val)) < 1e-6
+
+    # tanh, tanh(Value)
+    num1_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = tanh(num1)
+    backward(num2)
+
+    flux_tanh(a) = tanh(a)
+    grad = gradient(flux_tanh, num1_val)
+
+    @test abs(num1.grad - grad[1]) < 1e-6
+    @test abs(num2.data - tanh(num1_val)) < 1e-6
+end
+
+
