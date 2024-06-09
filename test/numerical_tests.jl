2f817d9c1ecb4ef6c1651bd99dff5b7812ad4623
Mike Saint-Antoine <msa406@nyu.edu>
2024-06-08 23:13:19 -0400

    add tests

---
+++ test/numerical_tests.jl
@@ -0,0 +1,259 @@
+using SimpleGrad
+using Test
+
+
+@testset "Value tests" begin
+
+    rand_lower = -20
+    rand_upper = 20
+
+    function random_value(lower, upper)
+        return lower + (upper - lower) * rand()
+    end
+
+
+    delta_var = 0.00000001
+
+
+    # Addition, Value + Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 + num2
+    backward(num3)
+
+
+    # Addition, numerical derivative calculation
+    num3_val = num1_val + num2_val
+    num3_new = (num1_val + delta_var) + num2_val
+    num1_grad = (num3_new - num3_val) / delta_var
+    num3_new = num1_val + (num2_val + delta_var)
+    num2_grad = (num3_new - num3_val) / delta_var
+
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+    # Subtraction, Value - Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 - num2
+    backward(num3)
+
+    # Subtraction, numerical derivative calculation
+    num3_val = num1_val - num2_val
+    num3_new = (num1_val + delta_var) - num2_val
+    num1_grad = (num3_new - num3_val) / delta_var
+    num3_new = num1_val - (num2_val + delta_var)
+    num2_grad = (num3_new - num3_val) / delta_var    
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+    # Multiplication, Value * Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 * num2
+    backward(num3)
+
+    # Multiplication, numerical derivative calculation
+    num3_val = num1_val * num2_val
+    num3_new = (num1_val + delta_var) * num2_val
+    num1_grad = (num3_new - num3_val) / delta_var
+    num3_new = num1_val * (num2_val + delta_var)
+    num2_grad = (num3_new - num3_val) / delta_var    
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+    # Division, Value / Value
+    num1_val = random_value(rand_lower, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = Value(num2_val)
+    num3 = num1 / num2
+    backward(num3)
+
+    # Division, numerical derivative calculation
+    num3_val = num1_val / num2_val
+    num3_new = (num1_val + delta_var) / num2_val
+    num1_grad = (num3_new - num3_val) / delta_var
+    num3_new = num1_val / (num2_val + delta_var)
+    num2_grad = (num3_new - num3_val) / delta_var    
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+    # e^x, exp(Value)
+    num1_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = exp(num1)
+    backward(num2)
+
+    # e^x, numerical derivative calculation
+    num2_val = exp(num1_val)
+    num2_new = exp(num1_val + delta_var)
+    num1_grad = (num2_new - num2_val) / delta_var
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num2.data - num2_val) < 1e-5
+
+
+    # natural log, log(Value)
+    num1_val = random_value(0, rand_upper)
+    num1 = Value(num1_val)
+    num2 = log(num1)
+    backward(num2)
+
+    # natural log, numerical derivative calculation
+    num2_val = log(num1_val)
+    num2_new = log(num1_val + delta_var)
+    num1_grad = (num2_new - num2_val) / delta_var
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num2.data - num2_val) < 1e-5
+
+
+    # x^c, Value^c
+    num1_val = random_value(0, rand_upper)
+    num2_val = random_value(rand_lower, rand_upper) * 0.1
+    num1 = Value(num1_val)
+    num3 = num1 ^ num2_val
+    backward(num3)
+
+    # x^c, numerical derivative calculation
+    num3_val = num1_val ^ num2_val
+    num3_new = (num1_val + delta_var) ^ num2_val
+    num1_grad = (num3_new - num3_val) / delta_var
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+    # tanh, tanh(Value)
+    num1_val = random_value(rand_lower, rand_upper)
+    num1 = Value(num1_val)
+    num2 = tanh(num1)
+    backward(num2)
+
+    # tanh, numerical derivative calculation
+    num2_val = tanh(num1_val)
+    num2_new = tanh(num1_val + delta_var)
+    num1_grad = (num2_new - num2_val) / delta_var
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num2.data - num2_val) < 1e-5
+end
+
+
+
+
+
+
+
+
+
+
+@testset "Tensor tests" begin
+
+
+    inputs_vals = rand(2, 3) * 10
+    weights1_vals = rand(3, 4)
+    weights2_vals = rand(4, 5)
+    biases1_vals = rand(4)
+    biases2_vals = rand(5) 
+    
+    
+    global inputs = Tensor(inputs_vals); # Matrix with shape (2,3) -- 2 batches, 3 input features per batch
+    global weights1 = Tensor(weights1_vals); # Matrix with shape (3,4) -- takes 3 inputs, has 4 neurons
+    global weights2 = Tensor(weights2_vals); # Matrix with shape (4,5) -- takes 4 inputs, has 5 neurons
+    global biases1 = Tensor(biases1_vals); # Bias vector for first layer neurons
+    global biases2 = Tensor(biases2_vals); # Bias vector for second layer neurons
+    
+    layer1_out = SimpleGrad.relu(inputs * weights1 + biases1);
+    layer2_out = layer1_out * weights2 + biases2;
+    
+    
+    y_true = [0 1 0 0 0;
+              0 0 0 1 0]
+    
+    
+    loss = softmax_crossentropy(layer2_out,y_true)
+    
+    backward(loss)
+    orig_loss = copy(loss.data)
+    
+    ###################################################################################################
+    delta_var = 0.00000001
+    
+    weights1_num_grad = zeros(size(weights1.grad))
+    for i in 1:size(weights1_num_grad)[1]
+        for j in 1:size(weights1_num_grad)[2]
+            weights1_new = deepcopy(weights1)
+            weights1_new.data[i,j] += delta_var
+            layer1 = SimpleGrad.relu(inputs * weights1_new + biases1);
+            layer2 = layer1 * weights2 + biases2;
+            loss_new = softmax_crossentropy(layer2,y_true)
+            delta_loss = loss_new.data - orig_loss
+            derivative = delta_loss[1] / delta_var[1]
+            weights1_num_grad[i,j] = derivative
+        end
+    end
+    
+    
+    weights2_num_grad = zeros(size(weights2.grad))
+    for i in 1:size(weights2_num_grad)[1]
+        for j in 1:size(weights2_num_grad)[2]
+            weights2_new = deepcopy(weights2)
+            weights2_new.data[i,j] += delta_var
+            layer1 = SimpleGrad.relu(inputs * weights1 + biases1);
+            layer2 = layer1 * weights2_new + biases2;
+            loss_new = softmax_crossentropy(layer2,y_true)
+            delta_loss = loss_new.data - orig_loss
+            derivative = delta_loss[1] / delta_var[1]
+            weights2_num_grad[i,j] = derivative
+        end
+    end
+    
+    biases1_num_grad = zeros(size(biases1.grad))
+    for i in 1:size(biases1_num_grad)[1]
+        biases1_new = deepcopy(biases1)
+        biases1_new.data[i] += delta_var
+        layer1 = SimpleGrad.relu(inputs * weights1 + biases1_new);
+        layer2 = layer1 * weights2 + biases2;
+        loss_new = softmax_crossentropy(layer2,y_true)
+        delta_loss = loss_new.data - orig_loss
+        derivative = delta_loss[1] / delta_var[1]
+        biases1_num_grad[i] = derivative
+    end
+    
+    biases2_num_grad = zeros(size(biases2.grad))
+    for i in 1:size(biases2_num_grad)[1]
+        biases2_new = deepcopy(biases2)
+        biases2_new.data[i] += delta_var
+        layer1 = SimpleGrad.relu(inputs * weights1 + biases1);
+        layer2 = layer1 * weights2 + biases2_new;
+        loss_new = softmax_crossentropy(layer2,y_true)
+        delta_loss = loss_new.data - orig_loss
+        derivative = delta_loss[1] / delta_var[1]
+        biases2_num_grad[i] = derivative
+    end
+    
+
+
+
+
+    @test all(abs.(weights1.grad .- weights1_num_grad) .< 1e-5)
+    @test all(abs.(weights2.grad .- weights2_num_grad) .< 1e-5)
+    @test all(abs.(biases1.grad .- biases1_num_grad) .< 1e-5)
+    @test all(abs.(biases2.grad .- biases2_num_grad) .< 1e-5)
+
+end
--- test/tensor_test.jl
+++ test/tensor_test.jl
@@ -2,14 +2,14 @@
 using SimpleGrad
 using Flux
 # using Flux.param
+using Test
 
 
-
-inputs_vals = rand(2, 3)
+inputs_vals = rand(2, 3) * 10
 weights1_vals = rand(3, 4)
 weights2_vals = rand(4, 5)
 biases1_vals = rand(4)
-biases2_vals = rand(5)
+biases2_vals = rand(5) 
 
 
 inputs = Tensor(inputs_vals); # Matrix with shape (2,3) -- 2 batches, 3 input features per batch
@@ -33,38 +33,100 @@ backward(loss)
 
 
 
-println(weights1.grad)
-println(weights2.grad)
-println(biases1.grad)
-println(biases2.grad)
+# println(weights1.grad)
+# println(weights2.grad)
+# println(biases1.grad)
+# println(biases2.grad)
 
-
+# println(loss)
 
 
 ##########################################################################################
 
 
 # Initialize inputs, weights, and biases
-inputs_vals = rand(2, 3)
-weights1_vals = rand(3, 4)
-weights2_vals = rand(4, 5)
-biases1_vals = rand(4)
-biases2_vals = rand(5)
+# inputs_vals = rand(2, 3)
+# weights1_vals = rand(3, 4)
+# weights2_vals = rand(4, 5)
+# biases1_vals = rand(4) * 10
+# biases2_vals = rand(5) * 10
 
 # # Define true labels (one-hot encoded)
 y_true = Flux.onehotbatch([2, 4], 1:5)
 
 # # Convert inputs and weights to Flux tensors
-inputs = Flux.params(inputs_vals)
-weights1 = Flux.params(weights1_vals)
-weights2 = Flux.params(weights2_vals)
-biases1 = Flux.params(biases1_vals)
-biases2 = Flux.params(biases2_vals)
+# inputs = Flux.params(inputs_vals)
+# weights1 = Flux.params(weights1_vals)
+# weights2 = Flux.params(weights2_vals)
+# biases1 = Flux.params(biases1_vals)
+# biases2 = Flux.params(biases2_vals)
+
+
+
+# inputs = inputs_vals
+# weights1 = weights1_vals
+# weights2 = weights2_vals
+# biases1 = biases1_vals
+# biases2 = biases2_vals
+
+
+# println(y_true)
+# println(size(y_true))
+
+
+
+test1 = SimpleGrad.relu(inputs * weights1 + biases1)
+test1 = test1 * weights2 + biases2
+y_true1 = [0 1 0 0 0;
+          0 0 0 1 0]
+
+
+exp_values = exp.(test1.data .- maximum(test1.data, dims=2))
+
+# exp_values = exp.(test1.data )
+
+println(maximum(test1.data, dims=2))
+
+exit(1)
+probs = exp_values ./ sum(exp_values, dims=2)
+
+## crossentropy - sample losses
+# samples = size(probs, 1)
+# probs_clipped = clamp.(probs, 1e-7, 1 - 1e-7)
+
+
+println(probs)
+println()
+
+# loss1 = softmax_crossentropy(test1,y_true1)
+
+
+# println(loss1)
+# println()
+
+test2 = max.(0, inputs_vals * weights1_vals .+ biases1_vals')
+test2 = test2 * weights2_vals .+ biases2_vals'
+y_true2 = Flux.onehotbatch([2, 4], 1:5)
+
+probs = Flux.softmax(test2)
+
+println(probs)
+
+# loss2 = Flux.crossentropy(Flux.softmax(test2'), y_true2)
+
+# println(loss2)
+
+
+
+# println(inputs)
+
+exit(1)
+
 
 # # Forward pass function
-function forward_pass(inputs, weights1, biases1, weights2, biases2)
-    layer1_out = max.(0, inputs * weights1 .+ biases1')
-    layer2_out = layer1_out * weights2 .+ biases2'
+function forward_pass(inputs_vals, weights1_vals, biases1_vals, weights2_vals, biases2_vals)
+    layer1_out = max.(0, inputs_vals * weights1_vals .+ biases1_vals')
+    layer2_out = layer1_out * weights2_vals .+ biases2_vals'
     return layer2_out
 end
 
@@ -74,21 +136,21 @@ function compute_loss(layer2_out, y_true)
 end
 
 # # Collect parameters
-ps = params(inputs,weights1, biases1, weights2, biases2)
+ps = Flux.params(inputs_vals,weights1_vals, biases1_vals, weights2_vals, biases2_vals)
 
 # # Compute the loss and gradients
-# loss, back = Flux.withgradient(ps) do
-#     layer2_out = forward_pass(inputs, weights1, biases1, weights2, biases2)
-#     compute_loss(layer2_out, y_true)
-# end
+loss, back = Flux.withgradient(ps) do
+    layer2_out = forward_pass(inputs_vals, weights1_vals, biases1_vals, weights2_vals, biases2_vals)
+    compute_loss(layer2_out, y_true)
+end
 
 # # Extract gradients
-# grad_weights1 = back[weights1]
-# grad_weights2 = back[weights2]
-# grad_biases1 = back[biases1]
-# grad_biases2 = back[biases2]
+grad_weights1 = back[weights1_vals]
+grad_weights2 = back[weights2_vals]
+grad_biases1 = back[biases1_vals]
+grad_biases2 = back[biases2_vals]
 
-# # Print the gradients
+# Print the gradients
 # println("Weights 1 gradients:")
 # println(grad_weights1)
 # println("Weights 2 gradients:")
@@ -99,3 +161,22 @@ ps = params(inputs,weights1, biases1, weights2, biases2)
 # println(grad_biases2)
 
 # println("done")
+
+
+
+println(loss)
+
+# @testset "Tensor tests" begin
+
+
+#     @test all(abs.(weights1.grad .- grad_weights1) .< 1e-6)
+#     # @test all(abs.(num2.grad .- grad[2]) .< 1e-6)
+#     # @test all(abs.(num3.data .- (num1_data * num2_data)) .< 1e-6)
+
+# end
+
+
+# println(weights1.grad)
+
+println()
+# println(grad_weights1)
