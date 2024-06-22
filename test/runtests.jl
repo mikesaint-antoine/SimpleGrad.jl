6a78564fa2fa6c2abe056327ef77c739cfaaab60
Mike Saint-Antoine <msa406@nyu.edu>
2024-06-21 20:10:51 -0400

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,8 @@ Random.seed!(1234)
     delta_var = 0.000000001
 
 
+    ######################################################################
+
     # Addition, Value + Value
     num1_val = random_value(rand_lower, rand_upper)
     num2_val = random_value(rand_lower, rand_upper)
@@ -37,6 +39,27 @@ Random.seed!(1234)
     @test abs(num2.grad - num2_grad) < 1e-5
     @test abs(num3.data - num3_val) < 1e-5
 
+
+    # Addition, Value + Number
+    num1.grad=0
+    num3 = num1 + num2_val
+    backward(num3)
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    # Addition, Number + Value
+    num2.grad=0
+    num3 = num1_val + num2
+    backward(num3)
+
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    ######################################################################
+
     # Subtraction, Value - Value
     num1_val = random_value(rand_lower, rand_upper)
     num2_val = random_value(rand_lower, rand_upper)
@@ -55,6 +78,28 @@ Random.seed!(1234)
     @test abs(num1.grad - num1_grad) < 1e-5
     @test abs(num2.grad - num2_grad) < 1e-5
     @test abs(num3.data - num3_val) < 1e-5
+
+
+
+    # Subtraction, Value - Number
+    num1.grad=0
+    num3 = num1 - num2_val
+    backward(num3)
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    # Subtraction, Number - Value
+    num2.grad=0
+    num3 = num1_val - num2
+    backward(num3)
+
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    ######################################################################
 
     # Multiplication, Value * Value
     num1_val = random_value(rand_lower, rand_upper)
@@ -75,6 +120,27 @@ Random.seed!(1234)
     @test abs(num2.grad - num2_grad) < 1e-5
     @test abs(num3.data - num3_val) < 1e-5
 
+
+    # Multiplication, Value * Number
+    num1.grad=0
+    num3 = num1 * num2_val
+    backward(num3)
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    # Multiplication, Number * Value
+    num2.grad=0
+    num3 = num1_val * num2
+    backward(num3)
+
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    ######################################################################
+
     # Division, Value / Value
     num1_val = random_value(rand_lower, rand_upper)
     num2_val = random_value(rand_lower, rand_upper)
@@ -94,6 +160,27 @@ Random.seed!(1234)
     @test abs(num2.grad - num2_grad) < 1e-5
     @test abs(num3.data - num3_val) < 1e-5
 
+
+    # Division, Value / Number
+    num1.grad=0
+    num3 = num1 / num2_val
+    backward(num3)
+
+    @test abs(num1.grad - num1_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    # Division, Number / Value
+    num2.grad=0
+    num3 = num1_val / num2
+    backward(num3)
+
+    @test abs(num2.grad - num2_grad) < 1e-5
+    @test abs(num3.data - num3_val) < 1e-5
+
+
+    ######################################################################
+
     # e^x, exp(Value)
     num1_val = random_value(rand_lower, rand_upper)
     num1 = Value(num1_val)
@@ -108,6 +195,7 @@ Random.seed!(1234)
     @test abs(num1.grad - num1_grad) < 1e-5
     @test abs(num2.data - num2_val) < 1e-5
 
+    ######################################################################
 
     # natural log, log(Value)
     num1_val = random_value(0, rand_upper)
@@ -124,20 +212,26 @@ Random.seed!(1234)
     @test abs(num2.data - num2_val) < 1e-5
 
 
-    # x^c, Value^c
-    num1_val = random_value(0, rand_upper)
-    num2_val = random_value(rand_lower, rand_upper) * 0.1
+    ######################################################################
+
+    # x^c, Value ^ Integer
+    num1_val = random_value(rand_lower, rand_upper)
     num1 = Value(num1_val)
-    num3 = num1 ^ num2_val
+    num2 = 2
+    num3 = num1 ^ num2
     backward(num3)
 
     # x^c, numerical derivative calculation
-    num3_val = num1_val ^ num2_val
-    num3_new = (num1_val + delta_var) ^ num2_val
+    num3_val = num1_val ^ num2
+    num3_new = (num1_val + delta_var) ^ num2
     num1_grad = (num3_new - num3_val) / delta_var
+ 
 
     @test abs(num1.grad - num1_grad) < 1e-5
     @test abs(num3.data - num3_val) < 1e-5
+
+
+    ######################################################################
 
     # tanh, tanh(Value)
     num1_val = random_value(rand_lower, rand_upper)
@@ -152,6 +246,7 @@ Random.seed!(1234)
 
     @test abs(num1.grad - num1_grad) < 1e-5
     @test abs(num2.data - num2_val) < 1e-5
+
 end
 
 
@@ -195,7 +290,7 @@ end
     ###################################################################################################
     delta_var = 0.00000001
     
-    weights1_num_grad = zeros(size(weights1.grad))
+    weights1_num_grad = zeros(size(weights1))
     for i in 1:size(weights1_num_grad)[1]
         for j in 1:size(weights1_num_grad)[2]
             weights1_new = deepcopy(weights1)
@@ -210,7 +305,7 @@ end
     end
     
     
-    weights2_num_grad = zeros(size(weights2.grad))
+    weights2_num_grad = zeros(size(weights2))
     for i in 1:size(weights2_num_grad)[1]
         for j in 1:size(weights2_num_grad)[2]
             weights2_new = deepcopy(weights2)
@@ -224,28 +319,32 @@ end
         end
     end
     
-    biases1_num_grad = zeros(size(biases1.grad))
+    biases1_num_grad = zeros(size(biases1))
     for i in 1:size(biases1_num_grad)[1]
-        biases1_new = deepcopy(biases1)
-        biases1_new.data[i] += delta_var
-        layer1 = SimpleGrad.relu(inputs * weights1 + biases1_new);
-        layer2 = layer1 * weights2 + biases2;
-        loss_new = softmax_crossentropy(layer2,y_true)
-        delta_loss = loss_new.data - orig_loss
-        derivative = delta_loss[1] / delta_var[1]
-        biases1_num_grad[i] = derivative
+        for j in 1:size(biases1_num_grad)[2]
+            biases1_new = deepcopy(biases1)
+            biases1_new.data[i,j] += delta_var
+            layer1 = SimpleGrad.relu(inputs * weights1 + biases1_new);
+            layer2 = layer1 * weights2 + biases2;
+            loss_new = softmax_crossentropy(layer2,y_true)
+            delta_loss = loss_new.data - orig_loss
+            derivative = delta_loss[1] / delta_var[1]
+            biases1_num_grad[i,j] = derivative
+        end
     end
     
-    biases2_num_grad = zeros(size(biases2.grad))
+    biases2_num_grad = zeros(size(biases2))
     for i in 1:size(biases2_num_grad)[1]
-        biases2_new = deepcopy(biases2)
-        biases2_new.data[i] += delta_var
-        layer1 = SimpleGrad.relu(inputs * weights1 + biases1);
-        layer2 = layer1 * weights2 + biases2_new;
-        loss_new = softmax_crossentropy(layer2,y_true)
-        delta_loss = loss_new.data - orig_loss
-        derivative = delta_loss[1] / delta_var[1]
-        biases2_num_grad[i] = derivative
+        for j in 1:size(biases2_num_grad)[2]
+            biases2_new = deepcopy(biases2)
+            biases2_new.data[i,j] += delta_var
+            layer1 = SimpleGrad.relu(inputs * weights1 + biases1);
+            layer2 = layer1 * weights2 + biases2_new;
+            loss_new = softmax_crossentropy(layer2,y_true)
+            delta_loss = loss_new.data - orig_loss
+            derivative = delta_loss[1] / delta_var[1]
+            biases2_num_grad[i,j] = derivative
+        end
     end
     
 
