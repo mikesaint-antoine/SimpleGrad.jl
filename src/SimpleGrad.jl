dcda09b3e0b7f60b47ba4ce1c678cd64e70e7694
Mike Saint-Antoine <msa406@nyu.edu>
2024-06-21 20:06:45 -0400

    rewrite with subtyping

--- src/SimpleGrad.jl
+++ src/SimpleGrad.jl
@@ -1,30 +1,36 @@
 module SimpleGrad
 export Value, Tensor, relu, softmax_crossentropy, backward
 
-
-###############################################################################################################################################
-###############################################################################################################################################
-###############################################################################################################################################
-###############################################################################################################################################
-## Value 
+# Base functions that we're going to be overriding 
+import Base: ==, show, +, *, exp, log, -, ^, inv, /, tanh
 
 
+# Operation
 struct Operation{FuncType,ArgTypes}
     op::FuncType
     args::ArgTypes
 end
 
-mutable struct Value{opType}
+
+# Value
+mutable struct Value{opType} <: Number
     data::Float64
     grad::Float64
     op::opType
 end
 
+
 # constructor -- Value(data, grad, op)
 Value(x::Number) = Value(Float64(x), 0.0, nothing)
 
-## defines what happens when we print out a Value
-import Base: show
+
+Base.promote_rule(::Type{<:Value}, ::Type{T}) where {T<:Number} = Value
+
+
+function ==(a::Value, b::Value)
+    return a===b
+end
+
 function show(io::IO, value::Value)
     print(io, "Value(",value.data, ")")
 end
@@ -32,124 +38,73 @@ end
 backprop!(val::Value{Nothing}) = nothing
 
 
-###############################################################################################################################################
-## addition
 
-import Base.+
+# addition
 function +(a::Value, b::Value)
+
     out = a.data + b.data
-
-    # Value(data, grad, op)
-    result = Value(out, 0.0, Operation(+, (a, b) ))
-
+    result = Value(out, 0.0, Operation(+, (a, b) )) # Value(data, grad, op)
     return result
-end
 
-# addition for Value + number
-function +(a::Value, b::Number)
-    b_value = Value(Float64(b)) # cast b to Value
-    return a + b_value  # use the existing method for Value + Value
-end
-
-# addition for number + Value
-function +(a::Number, b::Value)
-    return b + a # use Value + Number, which then casts the number to Value and does Value + Value
 end
 
 
-
-
-
-# backprop for addition operation
+# addition backprop
 function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(+), ArgTypes}
 
-    val.op.args[1].grad += val.grad
-
-    val.op.args[2].grad += val.grad
+    val.op.args[1].grad += val.grad # update gradient of first operand
+    val.op.args[2].grad += val.grad # update gradient of second operand
 
 end
 
 
-
-###############################################################################################################################################
-## multiplication
-
-import Base.*
+# multiplication
 function *(a::Value, b::Value)
+
     out = a.data * b.data
-
-    # Value(data, grad, op)
-    result = Value(out, 0.0, Operation(*, (a, b) ))
-
+    result = Value(out, 0.0, Operation(*, (a, b) )) # Value(data, grad, op)
     return result
+
 end
 
 
-# Value * number
-function *(a::Value, b::Number)
-    b_value = Value(Float64(b))  # cast b to Value
-    return a * b_value # use the existing method for Value * Value
-end
-
-# number * Value
-function *(a::Number, b::Value)
-    return b * a # use Value * Number, which then casts the number to Value and does Value * Value
-end
-
-
-
-# backprop for multiplication operation
+# multiplication backprop
 function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(*), ArgTypes}
 
     val.op.args[1].grad += val.op.args[2].data * val.grad
-
     val.op.args[2].grad += val.op.args[1].data * val.grad
 
 end
 
 
-
-###############################################################################################################################################
-## exp
-
-
 # e^x
-import Base.exp
 function exp(a::Value)
 
     out = exp(a.data)
-
-    # Value(data, grad, op)
-    result = Value(out, 0.0, Operation(exp, (a,) ))
-
+    result = Value(out, 0.0, Operation(exp, (a,) )) # Value(data, grad, op)
     return result
 
 end
 
-# backprop for exp operation
+# e^x backprop
 function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(exp), ArgTypes}
 
     val.op.args[1].grad += val.data * val.grad
 
 end
 
-###############################################################################################################################################
-## log
 
 
-import Base.log
+# log
 function log(a::Value)
 
     out = log(a.data)
-
-    # Value(data, grad, op)
-    result = Value(out, 0.0, Operation(log, (a,) ))
-
+    result = Value(out, 0.0, Operation(log, (a,) )) # Value(data, grad, op)
     return result
 
 end
 
-# backprop for log operation
+# log backprop
 function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(log), ArgTypes}
 
     val.op.args[1].grad += (1.0 / val.op.args[1].data) * val.grad
@@ -159,49 +114,43 @@ end
 
 
 
-###############################################################################################################################################
-## subtraction and negation
 
-
-import Base.-
-
-# negation
+# negation (no backprop needed, just turn it into multiplication)
 function -(a::Value)
+
     return a * -1
+
 end
 
-# subtraction: Value - Value
+# subtraction (no backprop needed, just turn it into negation and addition)
 function -(a::Value, b::Value)
+
     return a + (-b)
-end
 
-# Value - number
-function -(a::Value, b::Number)
-    b_value = Value(Float64(b))
-    return a - b_value
 end
 
 
-# number - Value
-function -(a::Number, b::Value)
-    return b - a
-end
 
 
-###############################################################################################################################################
-# x^c
+# exponents -- for now, exponent must be Integer and can't be Value or Float
+function ^(a::Value, b::Integer)
 
-
-import Base.^
-function ^(a::Value, b::Number)
     out = a.data ^ b
-
-    # Value(data, grad, op)
-    result = Value(out, 0.0, Operation(^, (a,b) ))
-
+    result = Value(out, 0.0, Operation(^, (a,b) )) # Value(data, grad, op)
     return result
+
 end
 
+function ^(a::Value, b::Number)
+    throw(MethodError(^, (Value, Number), "Exponentiation with non-integer exponents is not supported."))
+end
+
+function ^(a::Number, b::Value)
+    throw(MethodError(^, (Number, Value), "Exponentiation with Value as the exponent is not supported."))
+end
+
+
+# exponents backprop
 function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(^), ArgTypes}
 
     val.op.args[1].grad += val.op.args[2] * (val.op.args[1].data ^ (val.op.args[2] - 1)) * val.grad
@@ -209,19 +158,18 @@ function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typ
 end
 
 
-###############################################################################################################################################
-# need special case for x^(-1) in Julia
 
-import Base.inv
+
+# need special case for x^(-1) in Julia -- called "inv()"
 function inv(a::Value)
+
     out = 1.0 / a.data
-
-    # Value(data, grad, op)
-    result = Value(out, 0.0, Operation(inv, (a,) ))
-
+    result = Value(out, 0.0, Operation(inv, (a,) )) # Value(data, grad, op)
     return result
+
 end
 
+# inv() backprop
 function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(inv), ArgTypes}
 
     val.op.args[1].grad -= (1.0 / (val.op.args[1].data * val.op.args[1].data)) * val.grad
@@ -229,33 +177,26 @@ function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typ
 end
 
 
-###############################################################################################################################################
-# division: a / b = a * b^(-1)
-
-# for now both must be Values
-# TODO -- implement Value / number and number / Value at some point? 
-
-import Base./
+# division (no backprop needed, just turn it into inv() and multiplication)
+# a / b = a * b^(-1)
 function /(a::Value, b::Value)
-    return a * (b ^ -1)
+
+    return a * inv(b)
+
 end
 
 
-###############################################################################################################################################
-# tanh
 
-import Base.tanh
+# tanh
 function tanh(a::Value)
 
     out = (exp(2 * a.data) - 1) / (exp(2 * a.data) + 1)
-
-    # Value(data, grad, op)
-    result = Value(out, 0.0, Operation(tanh, (a,) ))
-
+    result = Value(out, 0.0, Operation(tanh, (a,) )) # Value(data, grad, op)
     return result
+
 end
 
-
+# tanh backprop
 function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typeof(tanh), ArgTypes}
 
     val.op.args[1].grad += (1 - val.data^2) * val.grad
@@ -263,9 +204,7 @@ function backprop!(val::Value{Operation{FunType, ArgTypes}}) where {FunType<:typ
 end
 
 
-###############################################################################################################################################
-## full backward pass
-
+## full backward pass for Value
 function backward(a::Value)
 
     function build_topo(v::Value, visited=Value[], topo=Value[])
@@ -294,123 +233,130 @@ function backward(a::Value)
 
 end
 
-###############################################################################################################################################
-###############################################################################################################################################
-###############################################################################################################################################
-###############################################################################################################################################
-## Tensor 
 
 
 
-mutable struct Tensor{opType}
-    data::Union{Array{Float64,1},Array{Float64,2}}
-    grad::Union{Array{Float64,1},Array{Float64,2}}
+
+
+
+
+# Tensor 
+mutable struct Tensor{opType} <: AbstractArray{Float64, 2}
+    data::Array{Float64, 2}
+    grad::Array{Float64, 2}
     op::opType
 end
 
 
-# constructor -- Tensor(data, grad, op)
-Tensor(x::Union{Array{Float64,1},Array{Float64,2}}) = Tensor(x, zeros(Float64, size(x)), nothing)
+# 2D constructor -- Tensor(data, grad, op)
+Tensor(x::Array{Float64,2}) = Tensor(x, zeros(Float64, size(x)), nothing)
+
+
+# 1D constructor - reshape to 2D (row vector or column vector) then call 2D constructor
+function Tensor(x::Array{Float64, 1}; column_vector::Bool=false)
+
+    if column_vector
+        # column vector - size (N,1)
+        data_2D = reshape(x, (length(x), 1))
+    else
+        # DEFAULT row vector - size (1,N)
+        data_2D = reshape(x, (1,length(x)))
+    end
+
+    Tensor(data_2D, zeros(Float64, size(data_2D)), nothing) # Tensor(data, grad, op)
+
+end
 
 
 
-import Base.show
 function show(io::IO, tensor::Tensor)
     print(io, "Tensor(",tensor.data, ")")
 end
 
-
 backprop!(tensor::Tensor{Nothing}) = nothing
 
-###############################################################################################################################################
 
-# multiplication
-
-
-
-import Base.*
-function *(a::Tensor, b::Tensor)
-
-    out = a.data * b.data
-
-    # Tensor(data, grad, op)
-    result = Tensor(out, zeros(Float64, size(out)), Operation(*, (a, b)))
-
-    return result
+function ==(a::Tensor, b::Tensor)
+    return a===b
 end
 
 
 
+
+Base.size(x::Tensor) = size(x.data)
+
+Base.getindex(x::Tensor, i...) = getindex(x.data, i...)
+
+Base.setindex!(x::Tensor, v, i...) = setindex!(x.data, v, i...)
+
+
+
+# matrix multiplication
+function *(a::Tensor, b::Tensor)
+
+    out = a.data * b.data
+    result = Tensor(out, zeros(Float64, size(out)), Operation(*, (a, b))) # Tensor(data, grad, op)
+    return result
+
+end
+
+
+# matrix multiplication backprop
 function backprop!(tensor::Tensor{Operation{FunType, ArgTypes}}) where {FunType<:typeof(*), ArgTypes}
 
     tensor.op.args[1].grad += tensor.grad * transpose(tensor.op.args[2].data)
-
     tensor.op.args[2].grad += transpose(tensor.op.args[1].data) * tensor.grad 
 
 end
 
-###############################################################################################################################################
-
-# addition
 
 
-# define addition for 2 Tensor objects
-import Base.+
+
+# matrix addition
 function +(a::Tensor, b::Tensor)
 
-    if length(size(a.data)) == length(size(b.data))
-        out = a.data .+ b.data
-    elseif length(size(a.data)) > length(size(b.data))
-        # a is 2D, b is 1D
-        out = a.data .+ transpose(b.data)
-    else
-        # a is 1D, b is 2D
-        out = b.data .+ transpose(a.data)
-    end
-
-    # Tensor(data, grad, op)
-    result = Tensor(out, zeros(Float64, size(out)), Operation(+, (a, b)))
-
+    # broadcasting happens automatically for row-vector
+    out = a.data .+ b.data
+    result = Tensor(out, zeros(Float64, size(out)), Operation(+, (a, b))) # Tensor(data, grad, op)
     return result
+
 end
 
 
-# backprop for addition operation
+# matrix addition backprop
 function backprop!(tensor::Tensor{Operation{FunType, ArgTypes}}) where {FunType<:typeof(+), ArgTypes}
 
-
-    if length(size(tensor.grad)) > length(size(tensor.op.args[1].data))
-        tensor.op.args[1].grad += dropdims(sum(tensor.grad, dims=1), dims=1) # need dropdims to make this size (x,) rather than size (1,x)
-    else
+    if size(tensor.grad) == size(tensor.op.args[1].data)
         tensor.op.args[1].grad += ones(size(tensor.op.args[1].data)) .* tensor.grad
+    else
+        # reverse broadcast
+        tensor.op.args[1].grad += ones(size(tensor.op.args[1].grad)) .* sum(tensor.grad,dims=1)
     end
 
-    if length(size(tensor.grad)) > length(size(tensor.op.args[2].data))
-        tensor.op.args[2].grad += dropdims(sum(tensor.grad, dims=1), dims=1) # need dropdims to make this size (x,) rather than size (1,x)
-    else
+    if size(tensor.grad) == size(tensor.op.args[2].data)
         tensor.op.args[2].grad += ones(size(tensor.op.args[2].data)) .* tensor.grad
+    else
+        # reverse broadcast
+        tensor.op.args[2].grad += ones(size(tensor.op.args[2].grad)) .* sum(tensor.grad,dims=1)
     end
 
 end
 
 
-###############################################################################################################################################
+
 
 # relu
-
 function relu(a::Tensor)
 
     out = max.(a.data,0)
-
-    # Tensor(data, grad, op)
-    result = Tensor(out, zeros(Float64, size(out)), Operation(relu, (a,)))
-
+    result = Tensor(out, zeros(Float64, size(out)), Operation(relu, (a,))) # Tensor(data, grad, op)
     return result
+
 end
 
 
 
-
+# relu backprop
 function backprop!(tensor::Tensor{Operation{FunType, ArgTypes}}) where {FunType<:typeof(relu), ArgTypes}
 
     tensor.op.args[1].grad += (tensor.op.args[1].data .> 0) .* tensor.grad
@@ -418,12 +364,8 @@ function backprop!(tensor::Tensor{Operation{FunType, ArgTypes}}) where {FunType<
 end
 
 
-###############################################################################################################################################
 
 # softmax_crossentropy
-
-
-# TODO - y_true must be regular 2D array, NOT tensor (might change later)
 function softmax_crossentropy(a::Tensor,y_true::Union{Array{Int,2},Array{Float64,2}}; grad::Bool=true)
 
     ## implementing softmax activation and cross entropy loss separately leads to very complicated gradients
@@ -436,11 +378,9 @@ function softmax_crossentropy(a::Tensor,y_true::Union{Array{Int,2},Array{Float64
     # softmax activation
     exp_values = exp.(a.data .- maximum(a.data, dims=2))
     probs = exp_values ./ sum(exp_values, dims=2)
-    
-    ## crossentropy - sample losses
-    samples = size(probs, 1)
+
     probs_clipped = clamp.(probs, 1e-7, 1 - 1e-7)
-    # deal with 0s
+    # deal with 0s and 1s
 
 
     # basically just returns an array with the probability of the correct answer for each batch
@@ -449,9 +389,7 @@ function softmax_crossentropy(a::Tensor,y_true::Union{Array{Int,2},Array{Float64
     # negative log likelihood
     sample_losses = -log.(correct_confidences)
 
-
-    # loss_mean
-    # out = [mean(sample_losses)]
+    # loss mean
     out = [sum(sample_losses) / length(sample_losses)]
 
 
@@ -466,40 +404,36 @@ function softmax_crossentropy(a::Tensor,y_true::Union{Array{Int,2},Array{Float64
         y_true_argmax = argmax(y_true, dims=2)
 
         a.grad = copy(probs)
+
         for samp_ind in 1:samples
+
             a.grad[samp_ind, y_true_argmax[samp_ind][2]] -= 1
             ## this syntax y_true_argmax[i][2] is just to get the column index of the true value
-        end
-        a.grad ./= samples
 
+        end
+
+        a.grad ./= samples
 
     end
 
-    # Tensor(data, grad, op)
-    result = Tensor(out, zeros(Float64, size(out)), Operation(softmax_crossentropy, (a,)))
+    # reshape out from (1,) to (1,1) 
+    out = reshape(out, (1, 1))
+
+    result = Tensor(out, zeros(Float64, size(out)), Operation(softmax_crossentropy, (a,))) # Tensor(data, grad, op)
 
     return result
+
 end
 
 
-
+# softmax_crossentropy backprop is empty because gradient is easier to calculate during forward pass
 function backprop!(tensor::Tensor{Operation{FunType, ArgTypes}}) where {FunType<:typeof(softmax_crossentropy), ArgTypes}
 
 end
 
 
 
-
-
-
-
-
-###############################################################################################################################################
-
-# full backward pass
-
-
-
+## full backward pass for Tensor
 function backward(a::Tensor)
 
     function build_topo(v::Tensor, visited=Tensor[], topo=Tensor[])
